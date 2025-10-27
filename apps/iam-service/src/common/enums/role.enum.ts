16bdb677a90df96a5c80e31b877ab16e9c38b6d4
Jonathan Cabrera Duran <cabraball222@gmail.com>
2025-10-26 23:55:03 -0500

    feat(iam): define User entity and roles enum

---
+++ apps/iam-service/src/common/enums/role.enum.ts
@@ -0,0 +1,9 @@
+// apps/iam-service/src/common/enums/role.enum.ts
+
+export enum Role {
+  MF_ADMIN = 'mf_admin',
+  MF_FINANZAS = 'mf_finanzas',
+  DUENO_TIENDA = 'dueno_tienda',
+  STAFF_TIENDA = 'staff_tienda',
+  PROVEEDOR = 'proveedor',
+}
