db641253f66752e56a3ecb65e60589083c4a62f5
Jonathan Cabrera Duran <cabraball222@gmail.com>
2025-10-27 20:39:05 -0500

    feat(inventory): setup TypeORM and database connection

--- apps/inventory-service/src/app.module.ts
+++ apps/inventory-service/src/app.module.ts
@@ -1,9 +1,35 @@
+// apps/inventory-service/src/app.module.ts
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Carga el módulo de Configuración (.env)
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    // 2. Configura la conexión a la base de datos
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Solo para desarrollo
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
