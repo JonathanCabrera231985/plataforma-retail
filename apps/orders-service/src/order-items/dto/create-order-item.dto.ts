d35d3c6bb30a34095478728ba0183cc178af8d35
Jonathan Cabrera Duran <cabraball222@gmail.com>
2025-10-29 20:32:45 -0500

    feat(orders): define CreateOrderDto and CreateOrderItemDto

--- apps/orders-service/src/order-items/dto/create-order-item.dto.ts
+++ apps/orders-service/src/order-items/dto/create-order-item.dto.ts
@@ -1,1 +1,19 @@
-export class CreateOrderItemDto {}
+// apps/orders-service/src/order-items/dto/create-order-item.dto.ts
+import { IsInt, IsNotEmpty, IsNumber, IsPositive, IsUUID, Min } from 'class-validator';
+
+export class CreateOrderItemDto {
+  @IsUUID()
+  @IsNotEmpty()
+  productId: string;
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  @IsNotEmpty()
+  quantity: number;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  @IsNotEmpty()
+  priceAtPurchase: number; // El precio al que se vendió
+}
--- apps/orders-service/src/orders/dto/create-order.dto.ts
+++ apps/orders-service/src/orders/dto/create-order.dto.ts
@@ -1,1 +1,20 @@
-export class CreateOrderDto {}
+// apps/orders-service/src/orders/dto/create-order.dto.ts
+import { Type } from 'class-transformer';
+import { IsArray, IsNotEmpty, IsUUID, ValidateNested } from 'class-validator';
+import { CreateOrderItemDto } from '../../order-items/dto/create-order-item.dto';
+
+export class CreateOrderDto {
+  @IsUUID()
+  @IsNotEmpty()
+  userId: string; // ID del usuario que compra
+
+  @IsUUID()
+  @IsNotEmpty()
+  locationId: string; // ID de la tienda donde se vende
+
+  @IsArray()
+  @ValidateNested({ each: true }) // Valida cada ítem del array
+  @Type(() => CreateOrderItemDto) // Especifica el tipo del DTO anidado
+  @IsNotEmpty()
+  items: CreateOrderItemDto[];
+}
