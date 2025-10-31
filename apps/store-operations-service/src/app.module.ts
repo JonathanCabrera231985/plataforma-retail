f9687ac704198225dff75f2fecdad1a779c8d23f
Jonathan Cabrera Duran <cabraball222@gmail.com>
2025-10-30 22:25:39 -0500

    feat(store-ops): setup TypeORM and database connection

--- apps/store-operations-service/src/app.module.ts
+++ apps/store-operations-service/src/app.module.ts
@@ -1,9 +1,33 @@
+// apps/store-operations-service/src/app.module.ts
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Importa ConfigModule aquí
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Dev only
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/store-operations-service/src/main.ts
+++ apps/store-operations-service/src/main.ts
@@ -1,8 +1,24 @@
+// apps/store-operations-service/src/main.ts
+
+import 'reflect-metadata'; // Importar al inicio
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3004; // Usa el puerto 3004
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  await app.listen(port);
+  console.log(`StoreOperationsService está corriendo en el puerto ${port}`);
 }
 bootstrap();
