0523514680d679ae47f626890c4c9f11ce1311a3
Jonathan Cabrera Duran <cabraball222@gmail.com>
2025-11-03 18:12:37 -0500

    feat(suppliers): add auth utilities (strategy, guard, decorator, enum)

---
+++ apps/suppliers-service/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,39 @@
+// apps/suppliers-service/src/auth/strategies/jwt.strategy.ts
+
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+type JwtPayload = {
+  sub: string;
+  email: string;
+  role: string;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    // 1. Obtener el secreto ANTES de llamar a super()
+    const secret = configService.get<string>('JWT_SECRET');
+
+    // 2. Validar que el secreto existe
+    if (!secret) {
+      throw new InternalServerErrorException(
+        'No se ha definido JWT_SECRET en el archivo .env de suppliers-service',
+      );
+    }
+
+    // 3. Llamar a super()
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+    });
+  }
+
+  // Esta estrategia solo valida el token y adjunta el payload
+  async validate(payload: JwtPayload) {
+    return { id: payload.sub, email: payload.email, role: payload.role };
+  }
+}
